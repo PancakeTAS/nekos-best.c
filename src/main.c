16264732f8f23a556d63b865ac7f2d9c5f2f7000
PancakeTAS <pancake@mgnet.work>
2024-02-19 23:29:33 +0100

    Add cleanup

--- src/main.c
+++ src/main.c
@@ -44,4 +44,40 @@ int main() {
         printf("  - URL: %s\n  Anime name: %s\n", list4.responses[i]->url, list4.responses[i]->source.gif->anime_name);
     }
     printf("\n");
+
+    // cleanup
+    for (size_t i = 0; i < list.len; i++) {
+        free(list.endpoints[i]->name);
+        free(list.endpoints[i]);
+    }
+
+    for (size_t i = 0; i < list2.len; i++) {
+        free(list2.responses[i]->url);
+        free(list2.responses[i]->source.png->artist_name);
+        free(list2.responses[i]->source.png->artist_href);
+        free(list2.responses[i]->source.png->source_url);
+        free(list2.responses[i]->source.png);
+        free(list2.responses[i]);
+    }
+
+    for (size_t i = 0; i < list3.len; i++) {
+        free(list3.responses[i]->url);
+        free(list3.responses[i]->source.gif->anime_name);
+        free(list3.responses[i]->source.gif);
+        free(list3.responses[i]);
+    }
+
+    for (size_t i = 0; i < list4.len; i++) {
+        free(list4.responses[i]->url);
+        free(list4.responses[i]->source.gif->anime_name);
+        free(list4.responses[i]->source.gif);
+        free(list4.responses[i]);
+    }
+
+    free(list.endpoints);
+    free(list2.responses);
+    free(list3.responses);
+    free(list4.responses);
+
+    return EXIT_SUCCESS;
 }
